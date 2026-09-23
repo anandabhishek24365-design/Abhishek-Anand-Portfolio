134cc05ddd596539defc79fa2e92cf24addad69c
anandabhishek24365-design <anandabhishek24365email.com>
2026-09-23 23:35:45 +0530

    Revert "Set website title to Abhishek Anand Portfolio"
    This reverts commit a2858f526953b168f04089b7d4a6fbff76f0b620.

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Abhishek Anand Portfolio"
+  const title = config.title ?? "Abhishek Anand | Software Developer Portfolio"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
